adceb66f7843fcf57a9fb53350ab4d6c04f3da36
Sanket Pawar <pawarsanket668@gmail.com>
2021-05-22 22:44:38 +0530

    second commit

--- src/components/Subtotal.js
+++ src/components/Subtotal.js
@@ -5,19 +5,20 @@ import '../styles/Subtotal.css'
 function Subtotal() {
    // console.log(value);
    const [{basket}, dispatch] = useStateValue();
+   let sum=0;
     const getCardTotal = basket =>{
        return basket?.reduce((amount,item) => item.price + amount, 0);
     }
    //const sum= basket?.reduce((amount,item) => item.price + amount,0);
    let value= getCardTotal(basket)
-   
+   sum= sum + parseInt(value);
    
     return (
         
         <div className="subtotal">
-            <h3> SubTotal({basket.length} items): {`${value}`}</h3>
+            <h3> SubTotal({basket.length} items): {`${sum/10}`}</h3>
 
-            <button className="checkout__button">Proceed to CheckOut</button>
+            {/* <button className="checkout__button">Proceed to CheckOut</button> */}
         </div>
     )
 }
